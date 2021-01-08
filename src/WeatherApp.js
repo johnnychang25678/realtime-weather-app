d91828293937523014bf3ac13a80ff278add9678
Johnny Chang <zjps3407@gmail.com>
2021-01-08 17:52:03 +0800

    add dark theme

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react'
 import styled from '@emotion/styled'
+import { ThemeProvider } from '@emotion/react'
 import WeatherIcon from './WeatherIcon'
 import sunriseAndSunsetData from './sunrise-sunset.json'
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg'
@@ -9,7 +10,7 @@ import { ReactComponent as LoadingIcon } from './images/loading.svg'
 
 // CSS in JS
 const Container = styled.div`
-  background-color: #ededed;
+  background-color: ${({ theme }) => theme.backgroundColor};
   height: 100%;
   display: flex;
   align-items: center;
@@ -18,19 +19,19 @@ const Container = styled.div`
 const WeatherCard = styled.div`
   position: relative;
   min-width: 360px;
-  box-shadow: 0 1px 3px 0 #999999;
-  background-color: #f9f9f9;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
   box-sizing: border-box;
   padding: 30px 15px;
 `
 const Location = styled.div`
   font-size: 28px;
-  color: #212121;
+  color: ${({ theme }) => theme.titleColor};
   margin-bottom: 20px;
 `
 const Description = styled.div`
   font-size: 16px;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 30px;
 `
 const CurrentWeather = styled.div`
@@ -40,7 +41,7 @@ const CurrentWeather = styled.div`
   margin-bottom: 30px;
 `
 const Temperature = styled.div`
-  color: #757575;
+  color: ${({ theme }) => theme.temperatureColor};
   font-size: 96px;
   font-weight: 300;
   display: flex;
@@ -54,7 +55,7 @@ const Airflow = styled.div`
   align-items: center;
   font-size: 16px;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 20px;
   svg {
     width: 25px;
@@ -67,7 +68,7 @@ const Rain = styled.div`
   align-items: center;
   font-size: 16px;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   svg {
     width: 25px;
     height: auto;
@@ -81,7 +82,7 @@ const Refresh = styled.div`
   font-size: 12px;
   display: inline-flex;
   align-items: flex-end;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
 
   svg {
     margin-left: 10px;
@@ -101,6 +102,25 @@ const Refresh = styled.div`
     }
   }
 `
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: '#121416',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#f9f9fa',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
+};
 
 const fetchCurrentWeather = () => {
   return fetch('https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-0DF52233-79B4-4659-A2EA-FD8F74BAF57E&locationName=臺北')
@@ -193,6 +213,8 @@ const WeatherApp = () => {
   })
   const { observationTime, locationName, description, temperature, windSpeed, humid, weatherCode, rainPossibility, comfortability, isLoading } = weatherElement
 
+  const [currentTheme, setCurrentTheme] = useState('light')
+
   const moment = useMemo(() => getMoment(locationName), [locationName])
 
   const fetchData = useCallback( // useCallback is to reserve function.
@@ -222,43 +244,49 @@ const WeatherApp = () => {
     fetchData()
   }, [fetchData])
 
+  useEffect(() => {
+    setCurrentTheme(moment === 'day' ? 'light' : 'dark')
+  }, [moment])
+
   return (
-    <Container>
-      {console.log('render: WeatherApp')}
-      <WeatherCard>
-        <Location>{locationName}</Location>
-        <Description>
-          {description}
-          {comfortability}
-        </Description>
-        <CurrentWeather>
-          <Temperature>
-            {Math.round(temperature)} <Celsius>°C</Celsius>
-          </Temperature>
-          <WeatherIcon
-            currentWeatherCode={weatherCode}
-            moment={moment || "day"} />
-        </CurrentWeather>
-        <Airflow>
-          <AirFlowIcon />
-          {windSpeed} m/h
+    <ThemeProvider theme={theme[currentTheme]}>
+      <Container>
+        {console.log('render: WeatherApp')}
+        <WeatherCard>
+          <Location>{locationName}</Location>
+          <Description>
+            {description}
+            {comfortability}
+          </Description>
+          <CurrentWeather>
+            <Temperature>
+              {Math.round(temperature)} <Celsius>°C</Celsius>
+            </Temperature>
+            <WeatherIcon
+              currentWeatherCode={weatherCode}
+              moment={moment || "day"} />
+          </CurrentWeather>
+          <Airflow>
+            <AirFlowIcon />
+            {windSpeed} m/h
         </Airflow>
-        <Rain>
-          <RainIcon />
-          {Math.round(rainPossibility)} %
+          <Rain>
+            <RainIcon />
+            {Math.round(rainPossibility)} %
         </Rain>
-        <Refresh onClick={fetchData}
-          isLoading={isLoading}>
-          最後觀測時間：
+          <Refresh onClick={fetchData}
+            isLoading={isLoading}>
+            最後觀測時間：
           {
-            new Intl
-              .DateTimeFormat('zh-TW', { hour: 'numeric', minute: 'numeric' })
-              .format(new Date(observationTime))
-          }
-          {isLoading ? <LoadingIcon /> : <RefreshIcon />}
-        </Refresh>
-      </WeatherCard>
-    </Container>
+              new Intl
+                .DateTimeFormat('zh-TW', { hour: 'numeric', minute: 'numeric' })
+                .format(new Date(observationTime))
+            }
+            {isLoading ? <LoadingIcon /> : <RefreshIcon />}
+          </Refresh>
+        </WeatherCard>
+      </Container>
+    </ThemeProvider>
   )
 }
 
